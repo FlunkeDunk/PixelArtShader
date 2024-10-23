df7679a50cbc33ef0c60fe94bc98003023c49c10
FlunkeDunk <Arthurblack@outlook.dk>
2024-10-24 00:17:12 +0200

    Added shader

---
+++ scenes/camera_3d.gd
@@ -0,0 +1,17 @@
+extends Camera3D
+@onready var pointer: Node3D = get_parent()
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_forward"):
+		pointer.rotation.x += -1 * delta
+	elif Input.is_action_pressed("move_backward"):
+		pointer.rotation.x += 1 * delta
+	else:
+		pointer.rotation.x += 0 * delta
+
+	if Input.is_action_pressed("move_left"):
+		pointer.rotation.y += 1 * delta
+	elif Input.is_action_pressed("move_right"):
+		pointer.rotation.y += -1 * delta
+	else:
+		pointer.rotation.y += 0 * delta
